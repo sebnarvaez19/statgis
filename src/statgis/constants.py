ac41e36522c279a1b32d47b52c6d50759c07a19a
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-07-28 20:59:14 -0500

    feat: API endpoint and functions default values

---
+++ src/statgis/constants.py
@@ -0,0 +1,8 @@
+from typing import Final, Sequence
+
+
+# TODO: SDK to fetch data for Python
+API_URL: Final[str] = "https://api-791856053294.us-central1.run.app/hidrometorological"
+DEFAULT_PERCENTILES: Final[Sequence[float]] = (
+    0.0, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0
+)
