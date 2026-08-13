5c477a7286221a35937d7f42c5997941f04f6ef6
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-08-13 15:03:13 -0500

    fix: Sitio Nuevo limit

--- src/statgis/hydrology/predict.py
+++ src/statgis/hydrology/predict.py
@@ -6,7 +6,7 @@ def rio_magdalena_tebsa_bquilla(l: float, limit: float = 0.05) -> float:
         return limit
     
 
-def rio_magdalena_sitio_nuevo(l: float, limit: float = 0.500) -> float:
+def rio_magdalena_sitio_nuevo(l: float, limit: float = 0.230) -> float:
     predicted_l = -0.795 + 0.456 * l
     if predicted_l > limit:
         return predicted_l
