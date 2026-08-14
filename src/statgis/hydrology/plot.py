cd0e4991553fed5a51525bef1b37bf5f01702efe
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-08-14 15:19:40 -0500

    feat: canal del dique backwater plot

--- src/statgis/hydrology/plot.py
+++ src/statgis/hydrology/plot.py
@@ -181,32 +181,41 @@ def magdalena_river_backwater(ax: Axes | None = None) -> Axes:
         predict.rio_magdalena_tebsa_bquilla(df.sort_values("datetime").iloc[-1]["datum"]),
         0,
     ])
-    for i, (name, distance) in enumerate(constants.BOCATOMAS_RÍO_MAGDALENA):
-        label = None if i != 0 else "Bocatomas"
-        ax.axvline(x=distance, color="gray", linestyle="--", linewidth=1, label=label)
-        ax.text(x=distance, y=6, s=name, rotation=90, ha="right", va="center", fontsize="small")
-    x_stations = list(map(lambda x: x[1], constants.ESTACIONES_RÍO_MAGDALENA))
-    ax.scatter(
-        x=x_stations,
-        y=np.full_like(x_stations, 0.98, dtype=float),
-        c="#BA8E23",
-        transform=ax.get_xaxis_transform(),
-        marker="v",
-        label="Estaciones IDEAM",
-    )
-    for name, distance in constants.ESTACIONES_RÍO_MAGDALENA:
-        ax.text(x=distance, y=0.98, s=name, ha="center", va="bottom", fontsize="small", transform=ax.get_xaxis_transform())
-    ax.fill_between(x.index, x[0.0], x[1.0], facecolor="C0", alpha=0.3, label="Variabilidad nivel")
-    ax.fill_between(x.index, x[0.1], x[0.9], facecolor="C0", alpha=0.3)
-    ax.fill_between(x.index, x[0.25], x[0.75], facecolor="C0", alpha=0.3)
-    ax.plot(x.index, x[0.5], color="C0")
+    ax = add_intakes_to_backwater_plot(ax, constants.BOCATOMAS_RÍO_MAGDALENA)
+    ax = add_stations_to_backwater_plot(ax, constants.ESTACIONES_RÍO_MAGDALENA)
+    ax = add_stripes_to_backwater_plot(ax, x, 109.5)
     ax.plot(x.index, t, color="black", label="Nivel actual")
-    ax.text(x=109.5, y=x[0.0].iloc[0], s="Min", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[0.1].iloc[0], s="10%", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[0.25].iloc[0], s="25%", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[0.5].iloc[0], s="50%", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[0.75].iloc[0], s="75%", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[0.9].iloc[0], s="90%", ha="left", va="center", fontsize="small")
-    ax.text(x=109.5, y=x[1.0].iloc[0], s="Max", ha="left", va="center", fontsize="small")
+    ax.xaxis.set_inverted(True)
+    return ax
+
+
+def canal_dique_backwater(ax: Axes | None) -> Axes:
+    """
+    Plot the backwater curve of the Canal del Dique.
+
+    Parameters
+    ----------
+    ax : Axes | None, optional
+        The axes to plot on, by default None.
+
+    Returns
+    -------
+    Axes
+        The axes with the backwater curve plotted.
+    """
+    ax = validation.check_ax(ax)
+    df = data.load_dataset(29037020)
+    x = pd.DataFrame({
+        0.000: df.quantile(constants.DEFAULT_PERCENTILES)["datum"],
+        31.30: df.quantile(constants.DEFAULT_PERCENTILES)["datum"].map(predict.canal_dique_villa_rosa)
+    }).T
+    t = pd.Series([
+        df.sort_values("datetime").iloc[-1]["datum"],
+        predict.canal_dique_villa_rosa(df.sort_values("datetime").iloc[-1]["datum"]),
+    ])
+    ax = add_intakes_to_backwater_plot(ax, constants.BOCATOMAS_CANAL_DIQUE)
+    ax = add_stations_to_backwater_plot(ax, constants.ESTACIONES_CANAL_DIQUE)
+    ax = add_stripes_to_backwater_plot(ax, x, 0.0)
+    ax.plot(x.index, t, color="black", label="Nivel actual")
     ax.xaxis.set_inverted(True)
     return ax
